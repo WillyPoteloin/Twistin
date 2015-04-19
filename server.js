11fc6b5987615e5db9055486ff625a6c0f47d7ee
willy <willypoteloin@yahoo.fr>
2015-04-19 18:18:20 +0200

    mise en place de l'identification expressjs

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
+var auth = require('http-auth');
 
 var app = express();
+
+// on créer las paramètres pour l'authentification
+var basic = auth.basic({
+    realm: "Identification requise !",
+    file: __dirname + "/.htpasswd"
+});
+app.use(auth.connect(basic));
 
 app.use('/', express.static(__dirname + '/public/'));
 
